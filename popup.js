708b084e12f29b3f4736851060ed1755f9742cde
Ana Almeida <57116592+anapalmeida@users.noreply.github.com>
2021-07-13 19:26:09 -0300

    First commit

---
+++ popup.js
@@ -0,0 +1,30 @@
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: setPageBackgroundColor,
+//     });
+//   });
+  
+//   // The body of this function will be executed as a content script inside the
+//   // current page
+//   function setPageBackgroundColor() {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//       document.body.style.backgroundColor = color;
+//     });
+//   }
+
+// getWords.addEventListener("input", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true})
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id},
+//     function: searchForWords(foo),
+//   })
+// })
+
+// function searchForWords(foo) {
+  
+// }
